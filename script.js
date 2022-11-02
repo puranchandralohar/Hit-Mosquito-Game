323f1e3b8f2f18392edd7973c73cf5902bcd29bf
puranchandralohar <clpuran95@gmail.com>
2022-11-02 12:55:53 +0530

    Getting Score

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 let mosquito = document.querySelector(".mosquito")
+let gameStart = document.querySelector('.game_start')
+let result = document.querySelector(".result");
+
+
+let score = 0;
+let isGameStart = false;
+
+gameStart.addEventListener("click",()=>{
+    isGameStart == true;
+    flyMosquito()
+})
 
 
 function flyMosquito(){
-    setInterval(()=>{
+    let time = setInterval(()=>{
         let moveX = Math.floor(Math.random()*300);
         let moveY = Math.floor(Math.random()*700);
         console.log(moveX,moveY);
         mosquito.style.top= `${moveX}px`;
         mosquito.style.left= `${moveY}px`;
     },1000)
+
 }
 
 
+function getScore(){
+    score ++;
+    result.innerText = score;
 
-mosquito.addEventListener('click',()=>{
-    flyMosquito()
-   
+}
+
+mosquito.addEventListener('click',()=>{    
+   getScore()
 })
